5bd1726d598a6456b0ebc3a9d845fc281f6a6bf3
Jonathan <jonatan.ortega@cpic.cr>
2024-10-31 18:14:45 -0600

    Add styles and new-page

--- src/app/pages/news-page/news-page.component.ts
+++ src/app/pages/news-page/news-page.component.ts
@@ -16,12 +16,12 @@ export class NewsPageComponent {
       name: 'CB650R'
     },
     {
-      url: 'https://powersports.honda.com/motorcycle/standard/-/media/products/family/cb650r/trims/trim-main/cb650r/2024/2024-cb650r-pearl_smoky_gray-1505x923.png',
-      name: 'CB650R'
+      url: 'https://powersports.honda.com/motorcycle/supersport/-/media/products/family/cbr1000rr/trims/trim-main/cbr1000rr-abs/2025/2025-cbr1000rr-abs-pearl_white-1505x923.png',
+      name: 'CBR1000RR ABS'
     },
     {
-      url: 'https://powersports.honda.com/motorcycle/standard/-/media/products/family/cb650r/trims/trim-main/cb650r/2024/2024-cb650r-pearl_smoky_gray-1505x923.png',
-      name: 'CB650R'
+      url: 'https://www.kovemoto.com/uploadfile/202308/3c1108fd9265.png',
+      name: '450RALLY'
     },
     {
       url: 'https://powersports.honda.com/motorcycle/standard/-/media/products/family/cb650r/trims/trim-main/cb650r/2024/2024-cb650r-pearl_smoky_gray-1505x923.png',
